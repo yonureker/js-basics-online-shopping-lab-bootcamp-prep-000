63bc1e870fd46588e553299576c2f6f46eb0df3a
Onur Eker <yonureker@gmail.com>
2017-11-29 23:20:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -68,10 +68,10 @@ function removeFromCart(itemX){
 }
 
 function placeOrder(ccNumber){
-	if (ccNumber == " "){
+	if (!ccNumber){
 		console.log ("Sorry, we don't have a credit card on file for you.");
 	} else {
-		console.log (`Your total cost is ${total()}, which will be charged to the card ${ccNumber}.`)
+		console.log (`Your total cost is $${total()}, which will be charged to the card ${ccNumber}.`)
 	}
 	cart.length = 0;
 }
