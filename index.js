052571167f4529117e6e73c1b3939dc21c94fbbb
Onur Eker <yonureker@gmail.com>
2017-11-29 20:59:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,13 +9,44 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+var cart = [];
+
+function getCart() {
+ return cart;
+}
+
+function setCart(c) {
+  cart = c;
+  return cart;
+}
+
+function addToCart(itemName){
+	var itemPrice = Math.floor(100 * Math.random());
+	var object = {[itemName]: itemPrice};
+	cart.push(object);
+	console.log(`${itemName} has been added to your cart.`);
+	return cart;
 }
 
 function viewCart() {
-  // write your code here
+  if (cart.length === 0){
+    console.log("Your shopping cart is empty.")
+  } else {
+      var message = "In your cart, you have "
+      for (var i = 0; i < cart.length; i++){
+        for (var value in cart[i]){
+          message += `${value} at $${cart[i][value]}`
+          if (i !== cart.length - 1){
+              message += ", "
+            } else {
+              message += "."
+            }
+          }
+        }
+      }
+  console.log(message)
 }
+
 
 function total() {
   // write your code here
